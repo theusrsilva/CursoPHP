a14b313e8aa108ebba35ac13972ab25c86425de8
Matheus <theusrsilva@gmail.com>
2019-07-14 15:14:41 -0300

    finalizado HTML CSS index

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/index.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>índice de exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="assets">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Módulo - 01</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 02</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 03</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 04</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 05</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 06</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 07</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 08</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo - 09</h3>
+                    <ul>
+                        <li><a href="#">Execício A</a></li>
+                        <li><a href="#">Execício B</a></li>
+                        <li><a href="#">Execício C</a></li>
+                        <li><a href="#">Execício D</a></li>
+                        <li><a href="#">Execício E</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="footer">
+        <a class="copyright">Copyright © <?php echo date('Y'); ?> Criado por Matheus Rocha, Todos os direitos reservados.</a>
+    </footer>
+</body>
+
+</html>
